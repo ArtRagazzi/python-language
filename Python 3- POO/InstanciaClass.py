61b1000e31310a2193787836f77c1edda54805a1
Artur O <ao.ragazzi@gmail.com>
2025-03-25 11:57:37 -0300

    Primeira Classe

---
+++ Python 3- POO/InstanciaClass.py
@@ -0,0 +1,16 @@
+class Movie:
+    name = "",
+    releaseYear = 0
+    includedPlan = False
+    note = 0
+    durationMinutes = 0
+    
+#Primeiro filme
+movie = Movie()
+movie.name = "Super Mario Bros"
+movie.releaseYear = 2023
+movie.includedPlan = True
+movie.note = 5
+movie.durationMinutes = 125
+
+print(movie)
---
+++ Python 3- POO/MovielClass.py
@@ -0,0 +1,7 @@
+class Movie:
+    name = "",
+    releaseYear = 0
+    includedPlan = False
+    note = 0
+    durationMinutes = 0
+    
