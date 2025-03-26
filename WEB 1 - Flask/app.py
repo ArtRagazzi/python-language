ed06669289b81bcaae53c2caace732da2abb994f
Artur O <ao.ragazzi@gmail.com>
2025-03-26 10:20:45 -0300

    Adiconando FLASK

---
+++ WEB 1 - Flask/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def principal():
+    conteudo1 = 'Manipulação de dados'
+    conteudo2 = 'Herança e Templates'
+    conteudo3 = 'Integração de APIS'
+    conteudo4 = 'Banco de Dados'
+
+    return render_template(
+        "index.html"
+        conteudo1 = conteudo1
+        conteudo2 = conteudo2
+        conteudo3 = conteudo3
+        conteudo4 = conteudo4                   
+                           )
+
+@app.route('/sobre')
+def sobre():
+    return render_template("sobre.html")
+
