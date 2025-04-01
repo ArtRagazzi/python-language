de2cebd06cba2a0cef988ee1d5ea9d8cdca3a87b
Artur O <ao.ragazzi@gmail.com>
2025-04-01 10:27:57 -0300

    16 - Utilizando Formulario na pagina HTML

--- WEB 3 - Django/livraria/views.py
+++ WEB 3 - Django/livraria/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .form import SignUpForm
 
 # Create your views here.
 def home(request):
@@ -31,4 +32,25 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html')
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Auth e Login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(
+                username = username,
+                password = password
+            )
+            login(request, user)
+            messages.success(
+                request,
+                "Você fez o login com sucesso com o novo usuario!"
+            )
+            return redirect('home')
+            
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
